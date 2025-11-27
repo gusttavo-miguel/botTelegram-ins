c499688ffa2eb1013a8694913cc970b87b28c71e
Gustavo Miguel Da Silva <gustavo.dsilva@mv.com.br>
2025-11-27 17:50:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/mv/st/ins/BotTelegramInsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mv.st.ins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTelegramInsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotTelegramInsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mv/st/ins/BotTelegramInsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.st.ins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTelegramInsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
